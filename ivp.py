15f9775986a8f370529e8f24eb4a2f7ac1847960
Bob Nikitich <pakrentos@gmail.com>
2019-11-05 09:45:59 +0300

    added checkboxes and accuracy slider, fixed plots

--- ivp.py
+++ ivp.py
@@ -1,60 +1,99 @@
 class Solution:
     
-    def __init__(self, x0 = 1, y0 = 0.5, X = 9):
+    def __init__(self, n=1, x0=1, y0=0.5, X=9):
         self.x0 = x0
         self.y0 = y0
         self.X = X
-        self.n = 10
+        self.n = n
+
+        self.x_grid = []
+        self.grid_step = (X - x0)/self.n
+        while x0 < X:
+            self.x_grid.append(x0)
+            x0 += self.grid_step
+        
+        self.y_grid_e = []
+        self.y_grid_exact = []
+        self.y_grid_eim = []
+        self.y_grid_rk = []
+
+        self.exact()
+        self.euler()
+        self.eulerim()
+        self.rungekutta()
+
+
+    def set_accuracy(self, n, x0=1, y0=0.5, X=9):
+        self.x0 = x0
+        self.y0 = y0
+        self.X = X
+        self.n = n
+
         self.x_grid = []
         self.grid_step = (X - x0)/self.n
         while x0 < X:
             self.x_grid.append(x0)
             x0 += self.grid_step
 
-    def accuracy(self, n):
-        self.n = n
-        x0 = self.x0
-        self.x_grid = []
-        self.grid_step = (self.X - self.x0)/self.n
-        while x0 < self.X:
-            self.x_grid.append(x0)
-            x0 += self.grid_step
+        self.y_grid_e = []
+        self.y_grid_exact = []
+        self.y_grid_eim = []
+        self.y_grid_rk = []
 
-    
-    def xgrid(self):
+        self.exact()
+        self.euler()
+        self.eulerim()
+        self.rungekutta()
+
+    def get_x_grid(self):
         return self.x_grid
+    
+    def get_y_grid_exact(self):
+        return self.y_grid_exact
+    
+    def get_y_grid_e(self):
+        return self.y_grid_e
+
+    def get_y_grid_eim(self):
+        return self.y_grid_eim
+
+    def get_y_grid_rk(self):
+        return self.y_grid_rk
 
     def func(self, x, y):
         return (y**2 - y)/x
 
+    def exact(self):
+        self.y_grid_exact.clear()
+        for x_i in self.x_grid:
+            self.y_grid_exact.append(1/(x_i+1))
+
     def euler(self):
-        data = []
+        self.y_grid_e.clear()
         y_i = self.y0
         for x_i in self.x_grid:
-            data.append(y_i)
-            y_i = y_i + self.grid_step*self.func(x_i, y_i)     
-        return data
+            self.y_grid_e.append(y_i)
+            y_i = y_i + self.grid_step*self.func(x_i, y_i)
 
     def eulerim(self):
-        data = []
+        self.y_grid_eim.clear()
         y_i = self.y0
         for x_i in self.x_grid:
-            data.append(y_i)
+            self.y_grid_eim.append(y_i)
             y_i = y_i + (self.grid_step/2)*(
                                         self.func(x_i, y_i) + self.func(
                                             x_i + self.grid_step, y_i + self.grid_step*self.func(x_i, y_i)
                                             )
                                         )
-        return data
 
     def rungekutta(self):
-        data = []
+        self.y_grid_rk
         y_i = self.y0
         for x_i in self.x_grid:
-            data.append(y_i)
+            self.y_grid_rk.append(y_i)
             k1 = self.func(x_i, y_i)
             k2 = self.func(x_i + self.grid_step/2, y_i + k1*self.grid_step/2)
             k3 = self.func(x_i + self.grid_step/2, y_i + k2*self.grid_step/2)
             k4 = self.func(x_i + self.grid_step, y_i + k3*self.grid_step)
             y_i = y_i + self.grid_step*(k1 + 2*k2 + 2*k3 + k4)/6
-        return data
+
--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, QPushButton, QCheckBox, QSlider, QLabel
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import *
 
 import random
 
@@ -21,35 +22,67 @@ class App(QMainWindow):
         self.title = 'Differential Equations Assignment'
         self.width = 700
         self.height = 400
+        self.button = QPushButton('Draw plot', self)
+        self.check_exact = QCheckBox('Exact solution plot', self)
+        self.check_e = QCheckBox('Euler approx plot', self)
+        self.check_eim = QCheckBox('Euler improved approx plot', self)
+        self.check_rk = QCheckBox('Runge-Kutta approx plot', self)
+        self.accuracy_label = QLabel('Accuracy (N): 50', self)
+        self.accuracy_slider = QSlider(Qt.Horizontal, self)
+        self.sol = Solution(n=100)
+        self.canvas = PlotCanvas(self, width=5, height=4)
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setFixedSize(self.width, self.height)
 
-        m = PlotCanvas(self, width=5, height=4)
-        m.move(0, 0)
-        euler_button = QPushButton('Euler solution', self)
-        euler_button.clicked.connect(m.plot)
-        euler_button.move(500, 0)
-        euler_button.resize(200, 50)
+        self.canvas.move(0, 0)
+        
+        self.button.clicked.connect(self.draw)
+        self.button.move(500, 0)
+        self.button.resize(200, 50)
 
-        eulerim_button = QPushButton('Euler Improved solution', self)
-        eulerim_button.move(500, 50)
-        eulerim_button.resize(200, 50)
+        self.check_exact.move(505, 50)
+        self.check_exact.resize(200, 25)
 
-        eulerim_button = QPushButton('Runge-Kutta solution', self)
-        eulerim_button.move(500, 100)
-        eulerim_button.resize(200, 50)
+        self.check_e.move(505, 75)
+        self.check_e.resize(200, 25)
+
+        self.check_eim.move(505, 100)
+        self.check_eim.resize(200, 25)
+
+        self.check_rk.move(505, 125)
+        self.check_rk.resize(200, 25)
+
+        self.accuracy_label.move(510, 160)
+        self.accuracy_label.resize(200, 25)
+
+        self.accuracy_slider.setRange(1, 100)
+        self.accuracy_slider.setValue(50)
+        self.accuracy_slider.setTickInterval(1)
+        self.accuracy_slider.move(510, 180)
+        self.accuracy_slider.resize(150, 50)
+        self.accuracy_slider.valueChanged.connect(self.accuracy_change)
 
         self.show()
-    
-        
 
+    def draw(self):
+        self.canvas.clr()
+        if self.check_exact.isChecked():
+            self.canvas.plot(self.sol.get_x_grid(), self.sol.get_y_grid_exact(), color='b-')
+        if self.check_e.isChecked():
+            self.canvas.plot(self.sol.get_x_grid(), self.sol.get_y_grid_e())
+        if self.check_eim.isChecked():
+            self.canvas.plot(self.sol.get_x_grid(), self.sol.get_y_grid_eim())
+        if self.check_rk.isChecked():
+            self.canvas.plot(self.sol.get_x_grid(), self.sol.get_y_grid_rk())
+    
+    def accuracy_change(self):
+        self.accuracy_label.setText('Accuracy (N): ' + str(self.accuracy_slider.value()) )
 
 class PlotCanvas(FigureCanvas):
-
-    sol = Solution()
 
     def __init__(self, parent=None, width=5, height=4, dpi=100, sol=Solution()):
         fig = Figure(figsize=(width, height), dpi=dpi)
@@ -62,14 +95,16 @@ class PlotCanvas(FigureCanvas):
                                    QSizePolicy.Expanding,
                                    QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
-        self.sol = sol
+        self.ax = self.figure.add_subplot(111)
+        self.ax.set_title('y\'=(y^2 - y)/x')
 
-    def plot(self):
-        self.sol.accuracy(100)
-        data = self.sol.rungekutta()
-        ax = self.figure.add_subplot(111)
-        ax.plot(self.sol.xgrid(), data, 'r-')
-        ax.set_title('y\'=(y^2 - y)/x')
+    def clr(self):
+        self.ax.clear()
+        self.ax.set_title('y\'=(y^2 - y)/x')
+        self.draw()
+
+    def plot(self, x_grid, y_grid, color='r-'):
+        self.ax.plot(x_grid, y_grid, color)
         self.draw()
 
 
